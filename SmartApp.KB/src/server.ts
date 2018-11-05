ab067494766f1d43b88f9d89f800403ff7a72e96
fferrante3 <f.ferrante3@studenti.unipi.it>
2018-11-05 17:48:01 +0100

    kb: Add websocket api

---
+++ SmartApp.KB/src/server.ts
@@ -0,0 +1,58 @@
+import * as WebSocket from 'ws';
+import * as kb from './kb';
+
+const port = 5666;
+
+// initialize the WebSocket server instance
+const wss = new WebSocket.Server({ port });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    // connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+        let reply = 'fail';
+
+        // log the received message and send it back to the client
+        console.log('received: %s', message);
+        try {
+            const j = JSON.parse(message);
+            switch (j.method) {
+                case 'register':
+                    reply = kb.register();
+                    break;
+                case 'addFact':
+                    // tslint:disable-next-line:max-line-length
+                    kb.addFact(j.params.idSource, j.params.infoSum, j.params.TTL, j.params.reli, j.params.revi, j.params.jsonFact);
+                    reply = 'done';
+                    break;
+                case 'removeFact':
+                    if (kb.removeFact(j.params.idSource, j.params.jsonReq)) {
+                        reply = 'done';
+                    }
+                    break;
+                case 'query':
+                    const res = kb.query(j.params.jsonReq);
+                    reply = JSON.stringify(res);
+                    break;
+                case 'subscribe':
+                    // tslint:disable-next-line:max-line-length
+                    const callback = (r: any) => {
+                        try { ws.send(JSON.stringify(r));
+                        } catch (e) { console.log(e); }
+                    } ;
+                    if (kb.subscribe(j.params.idSource, j.params.jsonReq, callback )) {
+                        reply = 'done';
+                    }
+                    break;
+                default:
+                    reply = 'function not defined';
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        ws.send(reply);
+    });
+
+});
+
+console.log('Server started at port ' + port);
