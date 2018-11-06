37208276450c1157eafae38d4ca2563643e0ba81
fferrante3 <f.ferrante3@studenti.unipi.it>
2018-11-06 09:43:55 +0100

    kb: Update python test script

--- SmartApp.KB/kbtest.py
+++ SmartApp.KB/kbtest.py
@@ -1,6 +1,4 @@
-import websocket
 import json
-import threading
 
 from kb import *
       
@@ -11,13 +9,11 @@ print(addFact(myID, "test", 1, 50, 'false', {"prova": 2}))
 print(addFact(myID, "test", 1, 50, 'false', {"prova": 3}))
 
 print(removeFact(myID, {"prova": 2}))
-print(query({"prova": "$x"}))
+print(queryBind({"prova": "$x"}))
 
 def callbfun(res):
     print("callback:")
     print(res)
 
-t = subscrThr(myID, {"prova": "$x"}, callbfun)
-t.start()
+subscribe(myID, {"prova": "$x"}, callbfun)
 addFact(myID, "test", 1, 50, 'false', {"prova": "callb"})
-t.join()
