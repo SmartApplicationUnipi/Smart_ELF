1a32cc92a9f7c9ddb314006677d7955ce8091e6d
fromgold <f.ferrante3@studenti.unipi.it>
2018-11-07 15:51:05 +0100

    kb: Added tests

--- SmartApp.KB/src/inferenceStub.ts
+++ SmartApp.KB/src/inferenceStub.ts
@@ -6,7 +6,7 @@ const databaseRule = [
     { _data:
         { body: [{subject: '$prof' , relation: 'teaches', object: '$course'  },
            {subject: '$course', relation: 'is in room',  object: '$room'}],
-        head: { subject: '$prof', relation: 'is in', object: '$room'}, }
+        head: { subject: '$prof', relation: 'is in', object: '$room'} },
     },
     { _data:
         { body: [ { emoCoords: { angry: 10, neutral: '$n', happy: '$h' } }],
@@ -23,7 +23,6 @@ export function checkRules(fact: object) {
 
 function checkRule(head: object, body: object[], fact: object) {
     // se il fatto è uno dei predicati nel body della regola
-    
     console.log('Sono dentro.');
     console.log(fact);
     console.log();
@@ -34,7 +33,6 @@ function checkRule(head: object, body: object[], fact: object) {
 
     let binds;
     for (const pred of body) {
-        
         // console.log('guardo il predicato');
         // console.log(pred);
         // console.log();
@@ -79,7 +77,6 @@ function checkRule(head: object, body: object[], fact: object) {
             binds = tempbinds;
             if (binds.length === 0) {
                  // ho fallito e la regola non va applicata() vai alla prossima REGOLA)
-                
                 // console.log('NON HO SOLUZIONI');
                 // console.log();
 
