61f055d02da38f256357dfe9b56e85034dfd9ca1
Andrea Cossu <cossu48@gmail.com>
2018-11-08 16:05:11 +0100

    Fixed indentation

--- SmartApp.ENLP/tte_service.py
+++ SmartApp.ENLP/tte_service.py
@@ -12,31 +12,31 @@ from constants import TAG_USER_TRANSCRIPT, TAG_USER_EMOTION
 global myID
 
 def read_from_KB(pattern):
-	"""
+    """
     Read a tuple from the KB
     """
-	# not needed here
+    # not needed here
     return
 
 def write_to_KB(fact):
     """
     Post a tuple to the KB
     """
+
     kb.addFact(myID, TAG_USER_EMOTION, 1, 100, False, fact)
-    return
 
 def callback(param):
-	"""
-	Assess user emotion fro a given sentence
-	"""
+    """
+    Assess user emotion fro a given sentence
+    """
     sentence = param[0]["$input"]
     fact = extract_emotion(sentence)
     write_to_KB(fact)
 
 def __main__():
-	"""
-	Registers the module and subscribe to transcripts tuples
-	"""
+    """
+    Registers the module and subscribe to transcripts tuples
+    """
     myID = kb.register()
     kb.subscribe(myID, {"TAG": TAG_USER_TRANSCRIPT, "text": "$input"}, callback) #from the 'text to speech' module
 
