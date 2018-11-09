5caa9eff269e722b9b83a03d8c2118e5ccf5a2c6
burlamix <burlami@gmail.com>
2018-11-09 16:56:58 +0100

    emotion recognizer

---
+++ SmartApp.AV/sentimental_analizer.py
@@ -0,0 +1,9 @@
+
+
+
+# comando python da teminale da questa cartella, prossimamente faccio un python che chiama il teminale 
+
+# python audioAnalysis.py regressionFile -i angry1.wav --model svm --regression  pyAudioAnalysis/pyAudioAnalysis/data/svmSpeechEmotion
+
+
+#prima bisogna installare la libreria
