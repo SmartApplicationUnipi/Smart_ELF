df29e4d822e2001d4c11b65a553f04f4687b5f33
burlamix <burlami@gmail.com>
2018-11-11 18:22:46 +0100

    definition of emotion function

--- SmartApp.AV/sentimental_analizer.py
+++ SmartApp.AV/sentimental_analizer.py
@@ -1,4 +1,11 @@
+import sys
 
+sys.path.insert(0, './pyAudioAnalysis/pyAudioAnalysis')
+sys.path.insert(0, './OpenVokaturi-3-0a/examples/')
+sys.path.insert(0, './OpenVokaturi-3-0a')
+
+from audioAnalysis import *
+#from my_OpenVokaWavMean_linux64.py import *
 
 
 # comando python da teminale da questa cartella, prossimamente faccio un python che chiama il teminale 
@@ -7,3 +14,13 @@
 
 
 #prima bisogna installare la libreria
+
+input_file = "data_audio/Trump_We_will_build_a_great_wall.wav"
+model = "svm"
+model_name ="pyAudioAnalysis/pyAudioAnalysis/data/svmSpeechEmotion"
+
+a = my_regressionFileWrapper(input_file, model, model_name)
+
+#a = my_vokaturi(input_file)
+
+print (a)
