014bb9065cda964313a92cbca73087ed3e0970fd
michele <mikele.c@outlook.it>
2018-11-13 22:58:00 +0100

    remove useless code

--- SmartApp.CV/online/tests/conftest.py
+++ SmartApp.CV/online/tests/conftest.py
@@ -1,13 +1,6 @@
 import pytest
 from online.SDK.face_client import Facepp_Client
 
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
 @pytest.fixture
 def client():
     client = Facepp_Client()
