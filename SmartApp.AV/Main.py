6a695993ff96d7778f22595d8366d4d5812bcc28
Alessio Gravina <gravina.alessio@gmail.com>
2018-11-14 16:53:25 +0100

    communication with HW

--- SmartApp.AV/Main.py
+++ SmartApp.AV/Main.py
@@ -1,12 +1,14 @@
 from multiprocessing import Queue, Process
-import 	communication
+import communication
 import STT
 
 
 if __name__ == '__main__':
-    HWPort_sample = "/microphone/samples"
+    HALAddress = "10.101.48.147"  # default
+    HALAddress = "10.101.31.10"
+    HALAudioPort = 2001  # default
     q = Queue()
-    communication_process = Process(target=communication.receiver, args=("/av/transcriber", HWPort_sample, q, True))
-    STT_process = Process(target=STT.speech_to_text, args=(q,False))
+    communication_process = Process(target=communication.receiver, args=(HALAddress, HALAudioPort, q, False))
+    STT_process = Process(target=STT.speech_to_text, args=(q,))
     STT_process.start()
     communication_process.start()
--- SmartApp.AV/communication.py
+++ SmartApp.AV/communication.py
@@ -3,13 +3,24 @@ from multiprocessing import Queue, Process
 import time
 from os import path
 import speech_recognition as sr
+import sys
+sys.path.insert(0, "../SmartApp.HAL")
+from Bindings import HALInterface
 
-def receiver(AV_port_name, HW_port_name, queue, testing):
+
+def myHandler(queue):
+    def handleAudioMessages(audioMessage):
+        print("Received audio:\n\tTimestamp:%d\n\tChannels:%d\n\tSampleRate:%d\n\tBitPerSample:%d\n\t%d bytes of data\n\n" %
+              (audioMessage.timestamp, audioMessage.channels, audioMessage.sampleRate, audioMessage.bitsPerSample, len(audioMessage.data)))
+        queue.put([audioMessage.timestamp, audioMessage.channels, audioMessage.sampleRate, audioMessage.bitsPerSample, audioMessage.data])
+    return handleAudioMessages
+
+
+def receiver(HALAddress, HALAudioPort, queue, testing):
     """
-    This function implements the communication with the microphone through YARP
-    framework, appending all the message received to a queue.
-    :param AV_port_name: name of the receiver port
-    :param HW_port_name: name of the sender port
+    This function implements the communication with the microphone, appending all the message received to a queue.
+    :param HALAddress: address of the endpoint
+    :param HALAudioPort: port of the endpoint
     :param queue: process shared queue
     :param testing:  if is true, read audio sample/register from mic
     """
@@ -17,8 +28,8 @@ def receiver(AV_port_name, HW_port_name, queue, testing):
         AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "demo/Trump_We_will_build_a_great_wall.wav")
 
         r = sr.Recognizer()
-
-        if(True):
+        mic = True
+        if mic:
             with sr.AudioFile(AUDIO_FILE) as source:
                 raw_audio = r.record(source)  # read the entire audio file
         else:
@@ -27,11 +38,11 @@ def receiver(AV_port_name, HW_port_name, queue, testing):
                 print("Say something!")
                 raw_audio = r.listen(source)
     else:
-        pass
-        #todo
+        # create interface object
+        hal = HALInterface(HALAddress=HALAddress, HALAudioPort=HALAudioPort)
 
-    timestamp = time.time()
-    queue.put([timestamp, raw_audio])
+        # Audio
+        audioID = hal.registerAsAudioReceiver(myHandler(queue))
 
-
-
+        while True:
+            time.sleep(1)
