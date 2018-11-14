bcfeb3e6834eaf76c0d3812cd5ab08328df22e46
Carlo <carloalessi94@gmail.com>
2018-11-14 15:30:49 +0100

    extracted method face_matching() to assign a new ID to face descriptors

--- SmartApp.CV/offline/offvision.py
+++ SmartApp.CV/offline/offvision.py
@@ -37,18 +37,9 @@ class OffVision:
         for rect in detected_faces:
             face_desc = self.compute_face_descriptor(frame, rect)
             # find the closest match, within threshold
-            min_distance = float('inf')
-            match_id = None
-            for person_id, person_desc in self.people.items():
-                distance = np.linalg.norm(person_desc - face_desc)
-                if distance < min_distance:
-                    min_distance = distance
-                    match_id = person_id
-            if min_distance > self.match_dist_threshold:
-                # no match, assign a new identifier
-                match_id = str(uuid.uuid4())
+            assigned_id = self.face_matching(face_desc)
             # update face descriptor
-            self.people[match_id] = face_desc
+            self.people[assigned_id] = face_desc
             # adjust rectangle
             rrect = {'top': max(rect.top(), 0),
                     'bottom': min(rect.bottom(), frame.shape[0]),
@@ -59,6 +50,26 @@ class OffVision:
             # add to detected dictionary
             detected_people[match_id] = {'rect': rrect, 'emo': emotion}
         return detected_people
+
+    def face_matching(self, face_desc):
+        """
+        Checks whether the face descriptor was already present (i.e. the face has been already seen before).
+        The face descriptor 'face_desc' gets a new ID if it is not similar to an existing descriptor,
+        it gets the most similar descriptor's ID otherwise.
+        :param face_desc: face descriptor
+        :return: previous ID if matching succeeded, new ID otherwise.
+        """
+        min_distance = float('inf')
+        match_id = None
+        for person_id, person_desc in self.people.items():
+            distance = np.linalg.norm(person_desc - face_desc)
+            if distance < min_distance:
+                min_distance = distance
+                match_id = person_id
+        if min_distance > self.match_dist_threshold:
+            # no match, assign a new identifier
+            match_id = str(uuid.uuid4())
+        return match_id
 
     def compute_face_descriptor(self, frame, rect):
         """
