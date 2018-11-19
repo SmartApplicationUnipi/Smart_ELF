eab482749d856c6b2d196ad23fbae41e53e2871b
dave94-42 <davide.serramazza@gmail.com>
2018-11-19 18:37:58 +0100

    translation from italian sentence to english added

--- SmartApp.ENLP/TteService_c.py
+++ SmartApp.ENLP/TteService_c.py
@@ -3,12 +3,13 @@ from interface_tags import TAG_USER_TRANSCRIPT, TAG_USER_EMOTION, PATH_TO_KB_MOD
 sys.path.insert(0, PATH_TO_KB_MODULE)
 import kb
 from tte import extract_emotion
-
+from italian import *
 
 class TteService:
 
     def __init__(self, kb_ID):
         self.kb_ID = kb_ID
+        self.watson_auth = watson_authentication()
         print("init tte Service")
 
     def read_from_KB(pattern):
@@ -31,13 +32,16 @@ class TteService:
         Assess user emotion from a given sentence
         """
         sentence = param[0][0]["$input"]
+        lang = param[0][0]["$lang"]
         print("callback TTE called")
+        if (lang == "it"):
+            sentence = tranlsate(sentence)
         fact = extract_emotion(sentence)
         self.write_to_KB(fact)
         return
 
     def start_service(self):
-        kb.subscribe(self.kb_ID, {"TAG": TAG_USER_TRANSCRIPT, "text": "$input"}, self.callback) #from the 'text to speech' module
+        kb.subscribe(self.kb_ID, {"TAG": TAG_USER_TRANSCRIPT, "text": "$input", "language": "$lang"}, self.callback) #from the 'text to speech' module
 
 if __name__ == "__main__":
     global myID
