3d8ad1c85e3601b27285121b71f6a0ef989807cc
fferrante3 <f.ferrante3@studenti.unipi.it>
2018-11-20 18:09:38 +0100

    kb: refactor command line python tool

--- SmartApp.KB/bindings/python/register.py
+++
@@ -1,7 +0,0 @@
-from kb import register, registerTagDoc, getTagDoc
-
-print(register({"prova1": 'p1', "prova2": 'p2'}))
-print(registerTagDoc({"prova1": 'doc prova 1', "prova2": 'doc prova 2'}))
-print(getTagDoc(['prova1', 'prova2', 'prova3']))
-
-print(register({"prova1": 'p1', "prova2": 'p2', "prova3" : "p3"}))
