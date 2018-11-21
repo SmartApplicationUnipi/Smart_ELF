8c46f1f7bf83762c5f2ba256953f068b7204067d
fferrante3 <f.ferrante3@studenti.unipi.it>
2018-11-21 10:51:04 +0100

    kb: fix command line tools

--- SmartApp.KB/bindings/python/commands.py
+++ SmartApp.KB/bindings/python/commands.py
@@ -6,12 +6,14 @@ from kb import KnowledgeBaseClient
 kb = KnowledgeBaseClient(True)
 
 if (sys.argv[1] == "query"): r = kb.queryBind(json.loads(sys.argv[2]))['details']
-elif (sys.argv[1] == "addfact"): r = kb.addFact(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
-elif (sys.argv[1] == "addrule"): r = kb.addRule(sys.argv[2], sys.argv[3], sys.argv[4])
-elif (sys.argv[1] == "removefact"): r = kb.removeFact(sys.argv[2], sys.argv[3])
-elif (sys.argv[1] == "removerule"): r = kb.removeRule(sys.argv[2], sys.argv[3])
-elif (sys.argv[1] == "updatefact"): r = kb.updateFactByID(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
-elif (sys.argv[1] == "registertags"): r = kb.registerTags(sys.argv[2])
-elif (sys.argv[1] == "tagdetail"): r = kb.getTagDetails(sys.argv[2])
-else: r = "invalid argument"
+elif (sys.argv[1] == "addfact"): r = kb.addFact(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], json.loads(sys.argv[6]))
+elif (sys.argv[1] == "addrule"): r = kb.addRule(sys.argv[2], sys.argv[3], json.loads(sys.argv[4]))
+elif (sys.argv[1] == "removefact"): r = kb.removeFact(sys.argv[2], json.loads(sys.argv[3]))
+elif (sys.argv[1] == "removerule"): r = kb.removeRule(sys.argv[2], json.loads(sys.argv[3]))
+elif (sys.argv[1] == "updatefact"): r = kb.updateFactByID(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], json.loads(sys.argv[7]))
+elif (sys.argv[1] == "registertags"): r = kb.registerTags(json.loads(sys.argv[2]))
+elif (sys.argv[1] == "tagdetails"): r = kb.getTagDetails(sys.argv[2:])
+else: 
+    r = "invalid argument"
+    print(sys.argv[2:])
 print(r)
