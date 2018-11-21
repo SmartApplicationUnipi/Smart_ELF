2999daaea8c32ffb94ef5284b4c5efa71ba666a7
Antonio Sisbarra <a.sisbarra@studenti.unipi.it>
2018-11-21 16:42:10 +0100

    Little changes in Tags

--- SmartApp.CRW/src/java/elf_kb_protocol/Tags.java
+++ SmartApp.CRW/src/java/elf_kb_protocol/Tags.java
@@ -1,16 +1,13 @@
 package elf_kb_protocol;
 
-import java.util.List;
 
 public class Tags {
 
-    private Tag crawler_tag;
-    private static final String desc_tag1 = "ProvaDesc1";
-    private static final String doc_tag1 = "ProvaDoc1";
+    private Tag rdf_tag;
 
     public Tags() {
 
-        crawler_tag = new Tag(desc_tag1, doc_tag1);
+        rdf_tag = new Tag("RdfDesc", "RdfDoc");
 
     }
 
