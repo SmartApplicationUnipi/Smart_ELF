53b8e439f3aad8e05f45d0a5551ec19b1bf55c0a
Andrea Bisbano <a.bisbano@studenti.unipi.it>
2018-11-22 22:42:54 +0100

    kb: revised debug message in order to make them useful (up to case atom : placeholder). ps: 4 is a good debug level.

--- SmartApp.KB/src/matcher.ts
+++ SmartApp.KB/src/matcher.ts
@@ -1,7 +1,8 @@
 import { Colors, Debugger } from './debugger';
 import { DataObject } from './kb';
+import { Logger } from './logger';
 
-const D: Debugger = new Debugger();
+const D: Debugger = new Debugger(4);
 
 export type Response = Map<object, object[]>;
 
@@ -46,20 +47,27 @@ class Matcher {
         this.initBinds = iBinds;
         for (const data of dataset) {
             this.currBinds = [...this.initBinds];
+            D.newLine(1);
             if (this.matchBind(this.outerQuery, this.outerSorted, data)) {
+                D.resetIndentation()
+                D.clogNoID(Colors.GREEN, 'RESULT', '', 'Match success!', 4);
                 matches.set(data, [...this.currBinds]);
+            } else {
+                D.resetIndentation()
+                D.clogNoID(Colors.RED, 'RESULT', '', 'Match failed!', 4);
             }
         }
         return matches;
     }
 
-    private sortAndMatch(query: any, data: any): boolean {
+    private sortAndMatch(query: any, data: object): boolean {
         const sorted = this.sort(query);
         return this.matchBind(query, sorted, data);
     }
 
-    private matchBind(query: any, sorted: SortMap, data: any): boolean {
-        D.newLine(5);
+    private matchBind(query: any, sorted: SortMap, data: object): boolean {
+        //        Logger.getInstance().info('Matcher', 'Start visit on object', data);
+
         for (let i = 0; i < this.BINDS_CAT; ++i) {
             switch (i) {
                 case this.ID_AA: {
@@ -180,42 +188,48 @@ class Matcher {
     }
 
     private matchAtomAtom(queryKey: string, queryValue: string, data: { [index: string]: any }): boolean {
-        D.clog(Colors.PINK, 'INFO', this.ID_AA, '', 'key =>' + queryKey, 5);
+        D.clog(Colors.PINK, 'KEY', this.ID_AA, '', 'key => ' + queryKey, 4);
+        D.clog(Colors.PINK, 'VAL', this.ID_AA, '', 'value => ' + queryValue, 4);
         if (!data.hasOwnProperty(queryKey) || queryValue !== data[queryKey]) {
-            D.clog(Colors.RED, 'FAIL', this.ID_AA, '', 'match failed', 2);
+            D.clog(Colors.RED, 'FAIL', this.ID_AA, '', 'BAD match on key `' +
+                queryKey + '\', found `' + data[queryKey] + '\' instead of `' + queryValue + '\'', 2);
             return false;
         }
-        D.clog(Colors.GREEN, 'OK', this.ID_AA, '', 'match succeded', 2);
+        D.clog(Colors.GREEN, 'OK', this.ID_AA, '', 'GOOD match on key `' + queryKey + '\' with value `' + queryValue + '\'', 2);
         return true;
     }
 
     private matchAtomObject(queryKey: string, queryValue: object, data: { [index: string]: any }): boolean {
-        D.clog(Colors.PINK, 'INFO', this.ID_AO, '', 'key => ' + queryKey, 5);
+        D.clog(Colors.PINK, 'KEY', this.ID_AO, '', 'key => ' + queryKey, 4);
+        D.clog(Colors.PINK, 'VAL', this.ID_AO, '', 'value => ' + queryValue, 4);
         if (!data.hasOwnProperty(queryKey) || !isObject(data[queryKey])) {
             // Se non ha la chiave, o ha la chiave ma non è un oggetto ESPLODI
             D.clog(Colors.RED, 'FAIL', this.ID_AO, '',
-                'it doesn\'t have the key, or it has it but it\'s not associated to an object ', 2);
+                'key `' + queryKey + '\' is not found, or it isn\'t associated to an object', 2);
             return false;
         }
-        D.clog(Colors.BLUE, 'INFO', this.ID_AO, '\t', 'Entering recursion', 5);
+        D.increaseIndentation();
+        D.clog(Colors.BLUE, 'INFO', this.ID_AO, '', 'Entering recursion', 5);
         // FIXME: renounce to debug to have tail recursion
         const result: boolean = this.sortAndMatch(queryValue, data[queryKey]);
-        D.clog(Colors.BLUE, 'INFO', this.ID_AO, '\t', 'Exit recursion', 5);
+        D.clog(Colors.BLUE, 'INFO', this.ID_AO, '', 'Exit recursion', 5);
+        D.decreaseIndentation();
         if (!result) {
-            D.clog(Colors.RED, 'FAIL', this.ID_AO, '', 'inner objects are different.', 2);
+            D.clog(Colors.RED, 'FAIL', this.ID_AO, '', 'Inner objects are different.', 2);
             return false;
         }
         return true;
     }
 
     private matchAtomPlaceholder(queryKey: string, queryValue: string, data: { [index: string]: any }): boolean {
-        D.clog(Colors.PINK, 'INFO', this.ID_AP, '', 'key => ' + queryKey, 5);
+        D.clog(Colors.PINK, 'VAL', this.ID_AP, '', 'key => ' + queryKey, 4);
+        D.clog(Colors.PINK, 'VAL', this.ID_AP, '', 'value => ' + queryValue, 4);
         if (!data.hasOwnProperty(queryKey)) {
-            D.clog(Colors.RED, 'FAIL', this.ID_AP, '', 'Key is not in the data', 2);
+            D.clog(Colors.RED, 'FAIL', this.ID_AP, '', 'Key `' + queryKey + '\' not found', 2);
             return false;
         }
         if (this.currBinds.length === 0) {
-            D.clog(Colors.BLUE, 'INFO', this.ID_AP, '', 'It\'s thefirst bind', 5);
+            D.clog(Colors.YELLOW, 'BIND', this.ID_AP, '', 'First bind: `' + queryValue + '\' -> `' + data[queryKey] + '\'', 4);
             const b: { [index: string]: string } = {};
             b[queryValue] = data[queryKey];
             this.currBinds.push(b);
@@ -223,23 +237,25 @@ class Matcher {
             for (let k = 0; k < this.currBinds.length; ++k) {
                 if (this.currBinds[k].hasOwnProperty(queryValue)) {
                     if (this.currBinds[k][queryValue] !== data[queryKey]) {
-                        D.clog(Colors.RED, 'FAIL', this.ID_AP, '', 'invalid bind', 2);
+                        D.clog(Colors.YELLOW, 'SBIND', this.ID_AP, '', 'Remove old bind: `' + queryValue + '\' -> `'
+                            + this.currBinds[k][queryValue] + '\', because we found the pair `' + queryKey + '\' : `' + data[queryKey] + '\'', 2);
                         delete this.currBinds[k];
                         continue;
                     } else {
-                        D.clog(Colors.GREEN, 'OK', this.ID_AP, '', 'already bound', 3);
+                        D.clog(Colors.GREEN, 'OK', this.ID_AP, '', 'Found a previous GOOD bind: `' + queryValue + '\' -> `' + data[queryKey] + '\'', 3);
                     }
                 } else {
-                    D.clog(Colors.GREEN, 'OK', this.ID_AP, '', 'add new bind in current bind list', 2);
+                    D.clog(Colors.YELLOW, 'BIND', this.ID_AP, '', 'Add bind: `' + queryValue + '\' -> `' + data[queryKey] + '\'', 2);
                     this.currBinds[k][queryValue] = data[queryKey];
                 }
             }
         }
-        return this.filterAndReturn(this.ID_PO);
+        return this.filterAndReturn(this.ID_AP);
     }
 
     private matchPlaceholderAtom(queryKey: string, queryValue: string, data: { [index: string]: any }): boolean {
-        D.clog(Colors.PINK, 'INFO', this.ID_PA, '', 'key => ' + queryKey, 5);
+        D.clog(Colors.PINK, 'KEY', this.ID_PA, '', 'key => ' + queryKey, 5);
+        D.clog(Colors.PINK, 'VAL', this.ID_PA, '', 'value => ' + queryValue, 5);
         const newBinds: { [index: string]: any }[] = [...this.currBinds];
         const dataKeys = Object.keys(data);
         for (let k = 0; k < newBinds.length || k === 0; ++k) {
@@ -270,11 +286,12 @@ class Matcher {
                 }
             }
         }
-        return this.filterAndReturn(this.ID_PO);
+        return this.filterAndReturn(this.ID_PA);
     }
 
     private matchPlaceholderObject(queryKey: string, queryValue: object, data: { [index: string]: any }): boolean {
-        D.clog(Colors.PINK, 'INFO', this.ID_PO, '', 'key => ' + queryKey, 5);
+        D.clog(Colors.PINK, 'KEY', this.ID_PO, '', 'key => ' + queryKey, 5);
+        D.clog(Colors.PINK, 'VAL', this.ID_PO, '', 'value => ' + queryValue, 5);
         const newBinds = [... this.currBinds];
         const dataKeys = Object.keys(data);
         let flag = false;
@@ -293,9 +310,11 @@ class Matcher {
                 && data.hasOwnProperty(newBinds[k][queryKey])
                 && isObject(data[newBinds[k][queryKey]])) {
                 D.clog(Colors.GREEN, 'OK', this.ID_PO, '', 'already existent correct bind', 3);
-                D.clog(Colors.BLUE, 'INFO', this.ID_PO, '\t', 'Entering recursion', 5);
+                D.increaseIndentation();
+                D.clog(Colors.BLUE, 'INFO', this.ID_PO, '', 'Entering recursion', 5);
                 const result = this.sortAndMatch(queryValue, data[newBinds[k][queryKey]]);
-                D.clog(Colors.BLUE, 'INFO', this.ID_PO, '\t', 'Exit recursion', 5);
+                D.clog(Colors.BLUE, 'INFO', this.ID_PO, '', 'Exit recursion', 5);
+                D.decreaseIndentation();
                 if (!result) {
                     D.clog(Colors.RED, 'FAIL', this.ID_PO, '', 'inner objects are different', 2);
                 } else {
@@ -319,9 +338,11 @@ class Matcher {
                 } else {
                     this.currBinds[k][queryKey] = dataKey;
                 }
-                D.clog(Colors.BLUE, 'INFO', this.ID_PO, '\t', 'Entering recursion', 5);
+                D.increaseIndentation();
+                D.clog(Colors.BLUE, 'INFO', this.ID_PO, '', 'Entering recursion', 5);
                 const result = this.sortAndMatch(queryValue, data[dataKey]);
-                D.clog(Colors.BLUE, 'INFO', this.ID_PO, '\t', 'Exit recursion', 5);
+                D.clog(Colors.BLUE, 'INFO', this.ID_PO, '', 'Exit recursion', 5);
+                D.decreaseIndentation();
                 if (!result) {
                     D.clog(Colors.RED, 'FAIL', this.ID_PO, '', 'inner objects are different', 2);
                 } else {
@@ -345,7 +366,7 @@ class Matcher {
     private filterAndReturn(id: number): boolean {
         this.currBinds = this.currBinds.filter(el => { return el != null });
         if (this.currBinds.length === 0) {
-            D.clog(Colors.YELLOW, 'EXIT', id, '', 'No more possible bind!', 4);
+            D.clog(Colors.RED, 'FAIL', id, '', 'No more possible bind!', 4);
             return false;
         }
         return true;
