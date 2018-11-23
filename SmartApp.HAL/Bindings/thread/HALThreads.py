41ff25b3e2ce4b533789585e48e40209e7983fca
Ruggiero-Santo <ruggiero.santo@gmail.com>
2018-11-23 20:45:35 +0100

    Added functions and attributes needed at vision_group

--- SmartApp.HAL/Bindings/thread/HALThreads.py
+++ SmartApp.HAL/Bindings/thread/HALThreads.py
@@ -19,7 +19,7 @@ class HALThread(Threading.Thread):
     Override the `handleSoc`
     """
 
-    def __init__(self, HALAddress, HALReadingPort, clientID, callback):
+    def __init__(self, HALAddress, HALReadingPort, clientID, callback, *args, **kwargs):
         Threading.Thread.__init__(self)
         # Config
         self.HALAddress = HALAddress
@@ -27,6 +27,7 @@ class HALThread(Threading.Thread):
         # Client managment
         self.clientID = clientID
         self.callback = callback
+        self.callback_params = {'args': args, 'kwargs': kwargs}
         # HALCommunication
         self.mustTerminate = False
         self.isConnected = False
@@ -71,7 +72,7 @@ class HALThread(Threading.Thread):
                         continue
 
                     if not(self.mustTerminate):
-                        self.callback(message)
+                        self.callback(message, *self.callback_params['args'], **self.callback_params['kwargs'])
 
                 except Socket.timeout as e:
                     Log.debug("HALThread for client %s. Socket timeouts %s" % (self.clientID, e))
@@ -132,3 +133,4 @@ class HALThread(Threading.Thread):
                 pass
             self.socket = None
         self.callback = None
+        self.callback_params = None
