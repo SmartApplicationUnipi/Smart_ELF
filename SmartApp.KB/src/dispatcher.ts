daef47597e548dbee0e47b0ebcd99cb2f04a6d62
lapotolo <lapotoloni@gmail.com>
2018-11-27 18:16:55 +0100

    dispatcher prototyper_v3. Need to test eval

--- SmartApp.KB/src/dispatcher.ts
+++ SmartApp.KB/src/dispatcher.ts
@@ -12,11 +12,11 @@ function isLower(params: string[]): boolean { return Number(params[0]) < Number(
 function isLowerEqual(params: string[]): boolean { return Number(params[0]) <= Number(params[1]); }
 
 // DATE PREDICATES
-function isEqualDate(params: string[]): boolean { return false; }
-function isAfterDate(params: string[]): boolean { return false; }
-function isBeforeDate(params: string[]): boolean { return false;  }
+function isEqualDate(params: string[]): boolean { return (new Date(params[0]).getTime()) ===  (new Date(params[1]).getTime()); }
+function isAfterDate(params: string[]): boolean {  return (new Date(params[0]).getTime()) >  (new Date(params[1]).getTime()); }
+function isBeforeDate(params: string[]): boolean {  return (new Date(params[0]).getTime()) < (new Date(params[1]).getTime());  }
 
-// GENERIC JAVASCRIPT CODE
+// GENERIC JAVASCRIPT CODE TO EXECUTE
 function evalFunction(functionCode: string): any {
     return eval(functionCode);
 }
@@ -27,6 +27,9 @@ const avaiableFunctions = {
     'isGreaterEqual': isGreaterEqual,
     'isLower': isLower,
     'isLowerEqual': isLowerEqual,
+    'isEqualDate': isEqualDate,
+    'isAfterDate': isAfterDate,
+    'isBeforeDate': isBeforeDate,
     // ...
     '\\lambda': evalFunction
 };
@@ -42,20 +45,16 @@ export function executeSpecialPredicate(functionName: string, params: string[][]
     return result;
 }
 
-console.log(executeSpecialPredicate('isEqual', [['50', '2'], ['2', '2']]));
+// TEST. SORRY FOR THAT
+// console.log("isEqual, expected [false, true]", executeSpecialPredicate('isEqual', [['50', '2'], ['2', '2']]));
+// console.log("isGreater, expected [true, false, false]", executeSpecialPredicate('isGreater', [['50', '2'], ['2', '2'], ['2', '50']]));
+// console.log("isGreaterEqual, expected [true, true]", executeSpecialPredicate('isGreaterEqual', [['50', '2'], ['2', '2']]));
+// console.log("isLower, expected [false, true]", executeSpecialPredicate('isLower', [['50', '2'], ['1', '2']]));
+// console.log("isLowerEqual, expected [true, true]", executeSpecialPredicate('isLowerEqual', [['2', '2'], ['1', '2']]));
+// console.log("\n");
+// console.log("equalDate, expected [true, false]", executeSpecialPredicate('isEqualDate', [['2018-03-11', '2018-03-11'], ['2018-03-10', '2018-03-11']]));
+// console.log("afterDate, expected [true, false]", executeSpecialPredicate('isAfterDate', [['December 17, 1995 03:24:01', 'December 17, 1995 03:24:00'], ['2018-03-09', '2018-03-11']]));
+// console.log("beforeDate, expected [true, true, false]", executeSpecialPredicate('isBeforeDate', [['December 17, 1994 02:24:01', 'December 17, 1995 03:24:00'], ['2010-03-09', '2018-03-11'], ['December 17, 2002 02:24:00', 'December 17, 2000 02:24:00']]));
 
 // const funkyCode: string[] = [];
 // console.log(executeSpecialPredicate('lambda', [funkyCode]));
-
-//function executeFunctionByName(functionName, context /*, args */) {
-//    var args = Array.prototype.slice.call(arguments, 2);
-//    var namespaces = functionName.split(".");
-//    var func = namespaces.pop();
-//    for(var i = 0; i < namespaces.length; i++) {
-//      context = context[namespaces[i]];
-//    }
-//    return context[func].apply(context, args);
-//  }
-
-//  executeFunctionByName("Namespace.functionName", My, arguments);
-//  executeFunctionByName("My.Namespace.functionName", window, arguments);
