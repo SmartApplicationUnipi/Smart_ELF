4ae27b490e0a0c2a5a4daf882b16cb1eff96d0a3
MickHardins <MickHardins@users.noreply.github.com>
2018-11-27 22:20:26 +0100

    tmp

--- SmartApp.QA/simple_queries.py
+++ SmartApp.QA/simple_queries.py
@@ -6,8 +6,37 @@ associated to each query t
 
 """
 
+"""queries with professor as last word"""
+query_prof = [
+    (1, "Dove posso trovare la lezione del Professor"),
+    (2, "Dov'è la lezione del Professore"),
+    (3, "Dove fa lezione il Professore"),
+    (4, "In che aula è la lezione del Professor"),
+    (5, "Dimmi l'aula della lezione del Professor"),
+    (6, "Dove devo andare per seguire la lezione del Professor"),
+    (7, "A che ora inizia la lezione del Professor"),
+    (8, "Sono in tempo per la lezione del Professor"),
+    (9, "A che ora finisce la lezione del Professor"),
+    (10, "Quando inizia la lezione del Professor"),
+    (11, "Quando farà lezione il Professor"),
+    (12, "Quando sarà la prossima lezione del professore"),
+    (13, "A che ora fa lezione il professore"),
+    (14, "In che giorno è la prossima lezione del professore"),
+    (15, "In che giorno fa lezione il professore"),
+    (16, "In che giorno ed a che ora è la prossima lezione del professore"),
+    (17, "Qual’è l’orario  settimanale del professore"),
+    (18, "DQual’è l’orario  settimanale delle lezioni del professore"),
+    (19, "Quali sono le lezioni del professore"),
+    (20, "In quale dipartimento fa ricevimento il professore"),
+    (21, "Dove è il ricevimento del professore"),
+    (22, "A che ora è il ricevimento del professore"),
+    (23, "Quando è il ricevimento del professore"),
+    (24, "In quale dipartimento è lo studio del professore"),
+    (25, "Qual’è lo studio del professore"),
+    (26, "Qual’è il numero dello studio del professore"),
+    (27, "Dove è lo studio del professore")    
+]
 
-lectures_place = [
-    (1,"Dove posso trovare la lezione del Professor C?"),
-    (2, "Dov'è la lezione del Professore C?")
+query_prof_t = [
+    (1, {"_data":{"subject":"Attardi", "relation":"$Y","object":"$Y"}})
 ]
---
+++ SmartApp.QA/templates.py
@@ -0,0 +1,16 @@
+"""
+Funzioni per l'analisi di frasi standard
+Each sentence will be lowercased
+ASt the init a dictionary is built:
+key: input_sentence
+value: (query to perform to kb,possible template answer to be filled)
+template answer will have 1 or more placeholder eg "Lecture of Attardi is in <aula>"
+"""
+
+def __main__():
+    input = "Dove si trova la Lezione del Professor Gervasi?"
+    template_list = [(1, "Dove si trova la lezione del professor <C>")]
+    input = input.lower()
+    temp_s = template_list[]
+    in_tokens 
+    
