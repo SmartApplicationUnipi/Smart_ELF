8bcc3a9752104a825752188cf2805f30ae166cf4
Desso95 <desso@hotmail.it>
2018-11-28 01:19:17 +0100

    First draft of rules to build DRS using NLTK parsers

--- SmartApp.QA/drs.py
+++ SmartApp.QA/drs.py
@@ -1,6 +1,9 @@
 from nltk.parse import load_parser
 from nltk.sem.drt import DrtParser
-parser = load_parser('rules.fcfg', trace=0, logic_parser=DrtParser())
+parser = load_parser('ELF_rules.fcfg', trace=0, logic_parser=DrtParser())
 
-for tree in parser.parse('un professore conosce una lezione'.split()):
+#remove punctuation ad go lowercase
+
+for tree in parser.parse('dove posso trovare la lezione del professor C'.split()):
+    #print(tree)
     print(tree.label()['SEM'].simplify())
