970af27c3631b7005ab68abf8ec8240b39864424
Desso95 <desso@hotmail.it>
2018-11-28 18:21:00 +0100

    almost semantically correct DRS

--- SmartApp.QA/drs.py
+++ SmartApp.QA/drs.py
@@ -1,9 +1,15 @@
 from nltk.parse import load_parser
 from nltk.sem.drt import DrtParser
 parser = load_parser('ELF_rules.fcfg', trace=0, logic_parser=DrtParser())
+#parser = load_parser('rules.fcfg', trace=0, logic_parser=DrtParser())
 
 #remove punctuation ad go lowercase
 
-for tree in parser.parse('dove posso trovare la lezione del professor C'.split()):
-    #print(tree)
-    print(tree.label()['SEM'].simplify())
+#sentence = 'a professor knows Mia'
+#sentence = 'dove posso trovare la lezione del professor Attardi'
+sentence = 'la lezione del professor Attardi'
+#sentence = 'Angus knows Irene'
+
+for tree in parser.parse(sentence.split()):
+    print(tree)
+    #print(tree.label()['SEM'].simplify())
