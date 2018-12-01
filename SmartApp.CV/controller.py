ee25166e9b94eb4b2678855173cc9a76e3661422
Ruggiero-Santo <ruggiero.santo@gmail.com>
2018-12-01 20:14:11 +0100

    Fixed and refactored controller (NOT TESTED)

--- SmartApp.CV/controller.py
+++ SmartApp.CV/controller.py
@@ -3,19 +3,16 @@ import external_modules.hal_client as hal
 from online.online_module import online_module as online
 from offline.offvision import OffVision as offline
 
-import numpy as np
-from threading import *
+from face_db import face_db as db
+
+from threading import Thread
 from queue import Queue
 import cv2
 
-from face_db import face_db as db
 
-import traceback
-
-
-#  Si possono avere più queue (almeno 6 dato che 6 è il numero massimo di facce
-#  riconosciute simultaneamente dal kinekt) che matengo in memoria i frame di
-#  ogni faccia che ha lo stesso id (dato da HAL-kinekt) cosi da poter avere
+#  IDEA: Si possono avere più queue (almeno 6 dato che 6 è il numero massimo di
+#  facce riconosciute simultaneamente dal kinekt) che matengo in memoria i frame
+#  di ogni faccia che ha lo stesso id (dato da HAL-kinekt) cosi da poter avere
 #  diversi thread che eseguono le richieste al modulo separatamente per ogni
 #  persona. Cosi facendo sarà necesssario eseguire l'identifica solo una volta,
 #  la prima volta, e non eseguirla più fino a quando non subentra una nuova
@@ -103,13 +100,11 @@ class Controller():
         self.db = db()
 
         # Ops for worker that compute all the analyzes
-        # TODO Creare 6 thread che lavoreano su più persone
         self.t = Thread(target=Controller._worker, args=[self, Controller.q])
         self.t.daemon = True
         self.t.start()
 
         # Initialization of exponential backoff machanism
-        # (note: for multithreading, this needs to be in the local storage!)
         self.attempt = 0
         self.exponent = 1
         self.max_exponent = 8
@@ -130,6 +125,35 @@ class Controller():
         for face in frame_obj.faces:
             Controller.q.put((face.img, frame_obj.frame_original_size))
 
+    def _get_id_person(fact, tuple):
+        if fact is not None and tuple is not None:
+            res = self.db.soft_get(tuple)
+            # res = [ [ tuple1, confidence1 ] ... [tuple_n, confidence_n] ]
+
+            if len(res) > 0: #something matches
+                vals = [res[0][0], res[0][1], True]
+
+            elif self.has_api_problem: #offline module case
+                vals = [self.db.insert(tuple), 0, False]
+
+            else: #online module case
+                descriptor = self.offline_module.get_descriptor(face_obj.img)
+
+                res = self.db.soft_get((descriptor, None))
+                if len(res) > 0: #something matches
+                    #return ID and update record with the token
+                    vals = [res[0][0], res[0][1], True]
+                    self.db[fact['personID']] = (descriptor, None)
+                else:
+                    #no match add it to db
+                    vals = [self.db.insert((descriptor, None)), 0, False]
+
+            atts = ['personID', 'confidence_identity', 'known']
+            for att, val in zip(atts, vals):
+                setattr(fact, att, val)
+
+        return fact
+
     def _worker(self, queue):
         """
             Function of thread that compute all the analyzes of frame.
@@ -143,45 +167,12 @@ class Controller():
                 if self.is_host:
                     face_obj, frame_size  = queue.get()
                     fact, tuple = self.watch(face_obj, frame_size)
+                    # tuple = (descriptor, token)
 
                     #########################################
                     #######     NOT TESTED    ###############
                     #########################################
-                    if fact is not None and tuple is not None:
-                        res = self.db.get(tuple)
-                        # res = [ [ tuple1, confidence1 ] ... [tuple_n, confidence_n] ]
-
-                        if len(res) > 0:
-                            #something matches
-                            fact['personID'] = res[0][0]
-                            fact['confidence_identity'] = res[0][1]
-                            fact['known'] = True
-
-                        elif self.has_api_problem:
-                            #offline module case
-                            fact['personID'] = self.db.insert(tuple)
-                            fact['confidence_identity'] = 0
-                            fact['known'] = False
-
-                        else:
-                            #online module case
-                            #compute descriptor
-                            descriptor = offline.get_descriptor(face_obj.img)
-                            #search descriptor
-                            res = self.db.get(( descriptor, None )
-
-                            if len(res) > 0:
-                                #something matches
-                                #return ID and update record with the token
-                                fact['personID'] = res[0][0]
-                                fact['confidence_identity'] = res[0][1]
-                                fact['known'] = True
-                                self.db.modify( descriptor, fact['personID'] )
-                            else:
-                                #no match add it to db
-                                fact['personID'] = self.db.insert(( descriptor, None ))
-                                fact['confidence_identity'] = 0
-                                fact['known'] = False
+                    fact = self._get_id_person(fact, tuple)
 
                     #self._add_fact_to_kb(fact)
                     queue.task_done()
@@ -196,65 +187,18 @@ class Controller():
         try:
             self._kb.addFact("face", tag, 1, jsonFact=fact, reliability=fact['confidence_identity'])
         except Exception as e:
-            print ("could not add fact", e)
+            print("Could not add fact", e)
 
-    def _getResolver(self):
-        """
-            Get the resolver of detection.
-
-            Params:
-
-            Return:
-                result (interface): Return an object that will resolve the detection
-                    of attributes and identity of person.
-        """
-        res = None
-        if not self.has_api_problem:
-            if self.online_module.is_available():
-                print("Using online vision module")
-                res = self.online_module
-                return res
-
-        if self.offline_module.is_available():
-            print("Using offline vision module")
-            res = self.offline_module
-            return res
-
-        if res == None:
-            raise Exception("Vision Module is not available")
-
-    def setAttr(self, *args, **kwargs):
-        """
-            Set attributes that the module must be detect form future frame.
-
-            Params:
-                depends on the module
-
-            Return:
-        """
-        self.online_module.set_detect_attibutes(*args, **kwargs)
-
-    def watch(self, face, frame_size = (0,0)):
-        img = face.img if hasattr(face, "img") else face
-        is_interlocutor = face.is_interlocutor if hasattr(face, "is_interlocutor") else False
-        face_position = face.face_position if hasattr(face, "face_position") else (0, 0)
-        z_index = face.z_index if hasattr(face, "z_index") else -1
-
-        # Exponential backoff mechanism
+    def _bake_off(img):
         try:
-            if self.attempt < 1:
-                # attempt an online analysis
+            if self.attempt < 1: # attempt an online analysis
                 fact, tuple = self.online_module.analyze_face(img)
-                # all fine, reset backoff
-                self.exponent = 1
-            else:
-                # we're in the backoff interval, work offline
+                self.exponent = 1 # all fine, reset backoff
+            else: # we're in the backoff interval, work offline
                 fact, tuple = self.offline_module.analyze_face(img)
-                # one more attempt done
-                self.attempt -= 1
+                self.attempt -= 1 # one more attempt done
                 # print('*** BACKOFF attempt', self.attempt, 'of exponent', self.exponent, '***')
         except:
-            traceback.print_exc()
             # something went wrong, double the attempt interval (until maximum length)
             if self.exponent < self.max_exponent:
                 self.exponent += 1
@@ -263,17 +207,26 @@ class Controller():
             # remeber, we've still to analyze this face:
             fact, tuple = self.offline_module.analyze_face(img)
 
-        if not fact:
-            print("Non vedo nessuno")
-        else:
+        return fact, tuple
+
+    def watch(self, face, frame_size = (0,0)):
+
+        fact, tuple = self._bake_off(face.img if hasattr(face, "img") else face)
+
+        if fact:
             look_at = {'pinch': 0, 'yaw':0}
             if set(frame_size)==0:
+                face_position = face.face_position if hasattr(face, "face_position") else (0, 0)
                 look_at = list( (p/(s/2))-1 for p,s in zip(face_position, frame_size))
                 look_at = {'pinch': look_at[0], 'yaw': look_at[1]}
 
-            fact.update({"look_at": look_at})
-            fact.update({"is_interlocutor": is_interlocutor})
-            fact.update({"z_index": z_index})
+            fact.update({
+                "look_at": look_at,
+                "is_interlocutor": face.is_interlocutor if hasattr(face, "is_interlocutor") else False,
+                "z_index": face.z_index if hasattr(face, "z_index") else -1,
+            })
+        else:
+            print("Non vedo nessuno")
 
         return fact, tuple
 
