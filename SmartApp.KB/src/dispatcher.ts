c800ed23947b799a9ce58e2bc8f25e0381cd2eec
lapotolo <lapotoloni@gmail.com>
2018-12-04 13:48:15 +0100

    added special predicates to support editDistance computations

--- SmartApp.KB/src/dispatcher.ts
+++ SmartApp.KB/src/dispatcher.ts
@@ -14,12 +14,39 @@ function isBeforeDate(op1: string, op2: string): boolean { return (new Date(op1)
 function isEqualString(op1: string, op2: string): boolean { return op1 === op2; }
 function isGreaterString(op1: string, op2: string): boolean { return op1 > op2; }
 function isLessString(op1: string, op2: string): boolean { return op1 < op2; }
+
+// EDIT DISTANCE PREDICATES
+function editDistanceEqual(w1: string, w2: string, num: string): boolean { return solveEditDistance(w1, w2) === Number(num); }
+function editDistanceGreater(w1: string, w2: string, num: string): boolean { return solveEditDistance(w1, w2) > Number(num); }
+function editDistanceLess(w1: string, w2: string, num: string): boolean { return solveEditDistance(w1, w2) < Number(num); }
+
+function solveEditDistance(word1: string, word2: string): number {
+    if (word1.length === 0 || word1 === undefined) { /*console.log('word1 is undefined');*/ return word2.length; }
+    if (word2.length === 0 || word2 === undefined) { /*console.log('word2 is undefined');*/ return word1.length; }
+    const memo: number[][] = [];
+    for (let i = 0; i <= word1.length; ++i) { memo[i] = new Array<number>(word2.length); }
+    for (let i = 0; i <= word1.length; ++i) { memo[i][0] = i; }
+    for (let j = 0; j <= word2.length; ++j) { memo[0][j] = j; }
+    for (let i = 1; i <= word1.length; ++i) {
+        for (let j = 1; j <= word2.length; ++j) {
+            if (word1[i - 1] === word2[j - 1]) { memo[i][j] = memo[i - 1][j - 1];
+            } else {
+                memo[i][j] = 1 + Math.min(
+                    memo[i - 1][j - 1],        // REPLACE
+                    Math.min(memo[i][j - 1],   // INSERT
+                        memo[i - 1][j]));      // REMOVE
+            }
+        }
+    }
+    // console.log('editDist('+ word1 + ', ' + word2 + ') = ', memo[word1.length][word1.length]);
+    return memo[word1.length][word1.length];
+}
 // GENERIC JAVASCRIPT CODE TO EXECUTE
 // export function evalFunction(functionCode: string[]): boolean {
 //     return eval("function"+(functionCode[0])+functionCode[:]);
 // }
 
-const avaiableFunctions: { [index: string]: (op1: string, op2: string) => boolean }
+const avaiableFunctions: { [index: string]: (op1: string, op2: string, ...otherParams: string[]) => boolean }
     = {
     'isEqual': isEqual,
     'isGreater': isGreater,
@@ -32,6 +59,9 @@ const avaiableFunctions: { [index: string]: (op1: string, op2: string) => boolea
     'isEqualString': isEqualString,
     'isGreaterString': isGreaterString,
     'isLessString': isLessString,
+    'editDistanceEqual': editDistanceEqual,
+    'editDistanceGreater': editDistanceGreater,
+    'editDistanceLess': editDistanceLess,
     // 'lambda': evalFunction
 };
 
@@ -46,6 +76,11 @@ export function executeSpecialPredicate(functionName: string, params: string[][]
 }
 
 // TEST. SORRY FOR THAT
+//console.log("editDistanceEqual, expected [false, true]", executeSpecialPredicate('editDistanceEqual', [['', 'lopo', 'lapo'], ['ciao', 'ciao', '0']]));
+// console.log("editDistanceLess, expected [false, true]", executeSpecialPredicate('editDistanceLess', [['', 'lopo', 'lapo'], ['caio', 'ciao', '3']]));
+// console.log("editDistanceGreater, expected [false, true]", executeSpecialPredicate('editDistanceGreater', [['lapo', 'lopo', '1'], ['lapo', 'alpo', '1']]));
+// console.log("editDistanceLess, expected [true, false]", executeSpecialPredicate('editDistanceLess', [['lapo', 'lopo', '2'], ['ciao', 'ciao', '0']]));
+// console.log("\n");
 // console.log("isEqual, expected [false, true]", executeSpecialPredicate('isEqual', [['50', '2'], ['2', '2']]));
 // console.log("isGreater, expected [true, false, false]", executeSpecialPredicate('isGreater', [['50', '2'], ['2', '2'], ['2', '50']]));
 // console.log("isGreaterEqual, expected [true, true]", executeSpecialPredicate('isGreaterEqual', [['50', '2'], ['2', '2']]));
@@ -61,5 +96,4 @@ export function executeSpecialPredicate(functionName: string, params: string[][]
 // console.log("functionNotSupported, expected [false, false]", executeSpecialPredicate('FOO', [['aba', 'lapo'], ['lapo', 'caro']]));
 
 // const funkyCode: string[] = [];
-
 // console.log(executeSpecialPredicate('lambda', [funkyCode]));
