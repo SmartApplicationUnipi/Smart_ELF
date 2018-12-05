a2e322174fb7625a70e7f2b1fee4dd47c0ed5085
Andrea Cossu <cossu48@gmail.com>
2018-12-05 16:40:06 +0100

    Modified rules

--- SmartApp.QA/addInferenceRules.py
+++ SmartApp.QA/addInferenceRules.py
@@ -3,17 +3,18 @@ from interface_tags import PATH_TO_KB_MODULE
 sys.path.insert(0, PATH_TO_KB_MODULE)
 from kb import KnowledgeBaseClient
 
-client= KnowledgeBaseClient(False)
+client = KnowledgeBaseClient(False)
+kb_id = (client.register())['details']
 
 rules = [
-    '{"teach": $prof, "room": $room, "course" : "$course" } <- \
-    {"name" : "$course", "teacher_name": "$prof2"}\
-    {"aula" : "$room", "descrizione" : "$course2"}\
-    ["containsString", ["$prof2", "$prof"]]\
-    ["containsString", ["$course2", "$course"]]'
+    '{"teach": "$prof", "room": "$room", "course" : "$course" } <- {"name" : "$course", "teacher_name": "$prof"};{"aula" : "$room", "descrizione" : "$course2"};["containsString", ["$course2", "$course"]]'
 ]
 for rule in rules:
-    client.addRule(124, "ENLP_EMOTIVE_ANSWER", rule)
+    x = client.addRule(kb_id, "ENLP_EMOTIVE_ANSWER", rule)
+    print(x)
 
-res = client.query({"_data": {"teach" : "Giuseppe Attardi", "room" : "$x"}})
+res = client.query({"teach" : "Giuseppe Attardi", "room" : "$x"})
 print(res)
+
+# DO NOT THIS ANYMORE
+#res = client.query({"_data": "$x"})
