dc63214fbb880adab90522b25b18ef9d2ee8731e
dave94-42 <davide.serramazza@gmail.com>
2018-12-05 15:08:36 +0100

    added teacher's info extracion

--- SmartApp.QA/Constant_from_kB.py
+++ SmartApp.QA/Constant_from_kB.py
@@ -14,18 +14,17 @@ class ConstantFromkB:
     def __init__(self, kb_ID, logging_lvl):
         self.logging_lvl = logging_lvl
         self.kb_ID = kb_ID
-        #logging.basicConfig(stream=sys.stderr, level=logging_lvl)
         logging.info('\tConstant_from_kB Service started')
         self.kb_client = KnowledgeBaseClient(True)
 
 
-    def extract_constant_from_KB(self,tag, file):
+    def extract_rooms_courses_from_KB(self,tag, file):
         """This method is the one devoted to extract "constants" information from the KB
-        First it perform a query to retrive the facts interested, then write them in rules.fcfg file
+        First it perform a query to retrieve the facts interested, then write them in file (2° parameter)
         """
 
         #answer query
-        answ = self.kb_client.query({"object": {"_data": {"tag": tag}}})
+        answ = self.kb_client.query({"_meta": {"tag": tag}})
         if answ['success'] == False:
             return
         else:
@@ -34,32 +33,56 @@ class ConstantFromkB:
         #get results
         ris = []
         for obj in answ['details']:
-            ris.append(obj['object']['_data']['object']['_data']['data']['name'])
+            ris.append(obj['object']['_data']['data']['name'])
 
         #write them in rules file
         for r in ris:
+            if (r==""):
+                continue
             if (tag==TAG_ROOM):
                 loc = "+LOC"
             else:
                 loc = "-LOC"
             #create string to write
-            str = "PropN[" + loc + ",NUM=sg,SEM=<\P.(DRS([x],[Attardi(x)])+P(x))>] -> " + "'" + r + "'" + "\n"
-            file.write(str)
+            str_tmp = str(r)+ ("(x)")
+            str_to_write = "PNOUN[SEM=<\\x.(DRS([],[" + str_tmp + "]))>] -> '" + str(r) + "'\n"
+
+            file.write(str_to_write)
+
+    def extract_teachers_from_KB(self,tag, file):
+        """This method is the one devoted to extract "constants" information from the KB
+        First it perform a query to retrieve the facts interested, then write them in file (2° parameter)
+        """
+
+        #answer query
+        answ = self.kb_client.query({"_meta": {"tag": tag}})
+        if answ['success'] == False:
+            return
+        else:
+            file.write("\n#section for " +tag + ":\n")
+
+        #write them in rules file
+        for r in answ["details"][0]["object"]["_data"]["data"]:
+            #create string to write
+            if (r==""):
+                continue
+            str_tmp = str(r)+ ("(x)")
+            str_to_write = "PNOUN[SEM=<\\x.(DRS([],[" + str_tmp + "]))>] -> '" + str(r) + "'\n"
+            file.write(str_to_write)
 
 
     def start(self):
         "ask for 'constants' facts"
 
         logging.info("\tConstant form Kb service started")
+
         #open file containing rules
-        rules_file = open("rules.fcfg", "a")
-        rules_file.write("\n\n#constants taken from KB\n")
+        rules_file = open("TEST_rules.fcfg", "a")
 
         #extraxt info from KB
-        self.extract_constant_from_KB(TAG_ROOM, rules_file)
-        self.extract_constant_from_KB(TAG_COURSE, rules_file)
-        self.extract_constant_from_KB(TAG_PROF, rules_file)
-
+        self.extract_teachers_from_KB(TAG_PROF,rules_file)
+        self.extract_rooms_courses_from_KB(TAG_ROOM, rules_file)
+        self.extract_rooms_courses_from_KB(TAG_COURSE, rules_file)
 
 
 if __name__ == "__main__":
