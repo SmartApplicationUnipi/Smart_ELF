d1df4fca6ca1dbbf9e8730effeac838f3c9a1083
Marco Cameriero <me@marcocameriero.net>
2018-12-11 00:48:21 +0100

    Beginning implementation of interaction logger.

--- tools/kb-analytics/src/routes.js
+++ tools/kb-analytics/src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const marked = require('marked');
 const hljs = require('highlight.js');
+const createError = require('http-errors');
 const router = express.Router();
+
+const logCollector = require('./logCollector');
 
 // Monkey patch the default markdown renderer to allow code highlighting
 const renderer = new marked.Renderer();
@@ -47,8 +50,34 @@ router.get('/docs/:source/:tag', async (req, res, next) => {
 });
 
 // GET /logs
-router.get('/logs', (req, res) => {
-    res.render('logs/index', { title: 'Logs' });
+router.get('/logs', async (req, res, next) => {
+    try {
+        res.render('logs/index', {
+            title: 'Logs',
+            interactions: await logCollector.getInteractions()
+        });
+    } catch (e) {
+        next(e);
+    }
+});
+
+// GET /logs/:id
+router.get('/logs/:id', async (req, res, next) => {
+    try {
+        const interaction = await logCollector.getInteraction(parseInt(req.params.id, 10));
+        if (!interaction) {
+            next(createError(404));
+            return;
+        }
+
+        res.render('logs/details', {
+            title: 'Interaction #' + req.params.id,
+            interaction,
+            details: await interaction.getDetails()
+        });
+    } catch (e) {
+        next(e);
+    }
 });
 
 module.exports = router;
