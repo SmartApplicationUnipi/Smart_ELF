5bfbb60f4706ec92a14181c08b728806072d7e7d
dave94-42 <davide.serramazza@gmail.com>
2018-12-13 19:28:48 +0100

    exception in drs.py

--- SmartApp.QA/drs.py
+++ SmartApp.QA/drs.py
@@ -1,8 +1,6 @@
-from nltk.parse import load_parser
 from nltk.sem.drt import DrtParser
 from nltk import *
 from interface_tags import TAG_DRS
-import logging
 
 def drs_matcher(s,p,QA_services,debug="no"):
     res = []
@@ -16,15 +14,12 @@ def drs_matcher(s,p,QA_services,debug="no"):
             elif(debug == "no"):
                 res.append(str(tree.label()['SEM'].simplify()))
     #if we have a failure in the parser tree, return False or rethrow exception
-    except ValueError:
-        logging.exception('ValueError')
+    except:
+        print('exception occurred')
         if(debug == "yes"): #forwarding output to do further testing when developing new grammar rules
             raise
         elif(debug == "no"):
             return False
-
-    except NotImplementedError:
-        logging.exception('NotImplementedError')
 
     if(debug == "yes"): #forwarding output to do further testing when developing new grammar rules
         return res
