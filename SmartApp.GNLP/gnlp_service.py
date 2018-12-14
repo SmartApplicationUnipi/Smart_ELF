68c3aa5647bec8b3f22e34b194bae67f3a83b58a
mellifluo <alefagnani@yahoo.it>
2018-12-14 01:18:02 +0100

    cleaning

--- SmartApp.GNLP/gnlp_service.py
+++ SmartApp.GNLP/gnlp_service.py
@@ -32,7 +32,7 @@ class GNLP_Service:
 		obj      = res[0]['details'][0]['object']['_data']
 		question = obj['text']
 		lang     = obj['language']
-		ts		 = obj['time_stamp']		
+		ts		 = obj['time_stamp']
 		print(question)
 		question = question['text']
 		luis_analysis = NLP_Understand(question, language = lang)
@@ -43,7 +43,7 @@ class GNLP_Service:
 			"entities": luis_analysis,
 			"dependencies": spacy_analysis,
 			"user_query": question,
-			"time_stamp": ts # TODO
+			"time_stamp": ts
 			}
 		)
 		# Logging some infos
@@ -69,7 +69,7 @@ class GNLP_Service:
 			"language" : lang,
 			"text": answer,
 			"user_query": question,
-			"time_stamp": ts # TODO
+			"time_stamp": ts
 			}
 		)
 
@@ -79,13 +79,9 @@ class GNLP_Service:
 		TAG_USER_TRANSCRIPT = "AV_IN_TRANSC_EMOTION"
 		TAG_CRW_RAW_INFO = "CRAWLER_DATA_ENTRY"
 		TAG_REASONER_OUTPUT = "REASONING_FRAME"
-		#self.KBC.subscribe(self.ID, {"_meta": {"_tag": TAG_USER_TRANSCRIPT}, "_data" : {"text": "?d} }, self.analyse)
-		self.KBC.subscribe(self.ID, {"_data": { "tag": TAG_USER_TRANSCRIPT ,"text": "$d", "language": "$lang"} }, self.analyse)
-		# self.KBC.subscribe(self.ID, {"_data" : "$d" }, self.analyse)
-		# self.KBC.subscribe(self.ID, {"_meta": {"_tag": TAG_CRW_RAW_INFO}, "_data" : {"data": "$input"} }, self.analyse)
-		# self.KBC.subscribe(self.ID, {"_meta": {"_tag": TAG_REASONER_OUTPUT}, "_data" : {"text": "$input"} }, self.answer)
 
-		#subscribe(self.ID, {"text_f_audio": "$input"}, self.callback)
+		self.KBC.subscribe(self.ID, {"_data": { "tag": TAG_USER_TRANSCRIPT ,"text": "$d", "language": "$lang"} }, self.analyse)
+		
 		print("Subscribed to the KB")
 
 
