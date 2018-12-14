SmartApplicationUnipi/Smart_ELF

commit 7c6eaf964d7e9ab3c3245ab7d24a52f037b5f44f

Added italian support for the LUIS analysis

SmartApp.GNLP/gnlp_service.py
@@ -29,12 +29,14 @@ class GNLP_Service:
 		print("Analysing...")
 
 		print(res)
-		question = res[0]['details'][0]['object']['_data']
+		obj      = res[0]['details'][0]['object']['_data']
+		question = obj['text']
+		lang     = obj['language']
 		print(question)
 		question = question['text']
-		luis_analysis = NLP_Understand(question)
+		luis_analysis = NLP_Understand(question, language = lang)
 		print('asd2')
-		spacy_analysis = get_dependency_tree(question)
+		spacy_analysis = get_dependency_tree(question, language = lang)
 		print('asd3')
 		self.KBC.addFact(self.ID, "NLP_ANALYSIS", 1, 50, {
 			"tag" : "NLP_ANALYSIS",
@@ -44,8 +46,6 @@ class GNLP_Service:
 			"time_stamp": 1 # TODO
 			}
 		)
-
-		print('asd4')
 		# Logging some infos
 
 		pp = pprint.PrettyPrinter()
@@ -79,7 +79,7 @@ class GNLP_Service:
 		TAG_CRW_RAW_INFO = "CRAWLER_DATA_ENTRY"
 		TAG_REASONER_OUTPUT = "REASONING_FRAME"
 		#self.KBC.subscribe(self.ID, {"_meta": {"_tag": TAG_USER_TRANSCRIPT}, "_data" : {"text": "?d} }, self.analyse)
-		self.KBC.subscribe(self.ID, {"_data": { "tag": TAG_USER_TRANSCRIPT ,"text": "$d"} }, self.analyse)
+		self.KBC.subscribe(self.ID, {"_data": { "tag": TAG_USER_TRANSCRIPT ,"text": "$d", "language": "$lang"} }, self.analyse)
 		# self.KBC.subscribe(self.ID, {"_data" : "$d" }, self.analyse)
 		# self.KBC.subscribe(self.ID, {"_meta": {"_tag": TAG_CRW_RAW_INFO}, "_data" : {"data": "$input"} }, self.analyse)
 		# self.KBC.subscribe(self.ID, {"_meta": {"_tag": TAG_REASONER_OUTPUT}, "_data" : {"text": "$input"} }, self.answer)

SmartApp.GNLP/nlp.py
@@ -16,7 +16,7 @@ def NLP_GetTemplate(intent):
 
 def NLP_Generate (JSON_Object):
 
-    placeholders, template = NLP_GetTemplate(JSON_Object['intent']) 
+    placeholders, template = NLP_GetTemplate(JSON_Object['intent'])
     for el in placeholders:
 
         key = el[1:-1]
@@ -26,14 +26,18 @@ def NLP_Generate (JSON_Object):
 
 
 
-def NLP_Understand (string, language = "en"): 
+def NLP_Understand (string, language = "en"):
 
-    if language != "en":
-        raise NotImplementedError("Language not implemented yet!")
-    
+    endpoint = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/118d13b3-b1fe-4ed5-be14-b48ca355935f'
+    sub_key  = 'f92fd052225848cca5de626fd874a7b5'
+
+    if language == "it":
+        endpoint = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c2ccb574-0462-4711-8099-786a67ee4eba'
+        sub_key  = 'f92fd052225848cca5de626fd874a7b5'
+
     headers = {
         # Request headers
-        'Ocp-Apim-Subscription-Key': 'f92fd052225848cca5de626fd874a7b5',
+        'Ocp-Apim-Subscription-Key': sub_key,
     }
 
     params ={
@@ -47,14 +51,14 @@ def NLP_Understand (string, language = "en"):
     }
 
     try:
-        r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/118d13b3-b1fe-4ed5-be14-b48ca355935f',headers=headers, params=params)
+        r = requests.get(endpoint,headers=headers, params=params)
         return r.json()
-    
+
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
         raise e
 
- 
+
 
 if __name__ == '__main__':
 

commit 09d89113c423c010871513751044332e0cdb96c0

fixed one more bug introduced in 7c6eaf964d7e9ab3c3245ab7d24a52f037b5f44f

SmartApp.GNLP/gnlp_service.py
@@ -34,7 +34,7 @@ class GNLP_Service:
 		lang     = obj['language']
 		ts		 = obj['timestamp']
 		print(question)
-		question = question['text']
+		#question = question['text']
 		luis_analysis = NLP_Understand(question, language = lang)
 		spacy_analysis = get_dependency_tree(question, language = lang)
 		self.KBC.addFact(self.ID, "NLP_ANALYSIS", 1, 50, {