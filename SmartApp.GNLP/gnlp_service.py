SmartApplicationUnipi/Smart_ELF

commit 08863d6d44f664c1dc47045393d9a505a2d47211

added time stamp and language tag

SmartApp.GNLP/gnlp_service.py
@@ -32,18 +32,18 @@ class GNLP_Service:
 		obj      = res[0]['details'][0]['object']['_data']
 		question = obj['text']
 		lang     = obj['language']
+		ts		 = obj['time_stamp']		
 		print(question)
 		question = question['text']
 		luis_analysis = NLP_Understand(question, language = lang)
-		print('asd2')
 		spacy_analysis = get_dependency_tree(question, language = lang)
-		print('asd3')
 		self.KBC.addFact(self.ID, "NLP_ANALYSIS", 1, 50, {
 			"tag" : "NLP_ANALYSIS",
+			"language": lang,
 			"entities": luis_analysis,
 			"dependencies": spacy_analysis,
 			"user_query": question,
-			"time_stamp": 1 # TODO
+			"time_stamp": ts # TODO
 			}
 		)
 		# Logging some infos
@@ -53,9 +53,9 @@ class GNLP_Service:
 		pp.pprint(spacy_analysis)
 		print(question)
 
-		self.answer(question)
+		self.answer(question, lang, ts)
 
-	def answer(self, question):
+	def answer(self, question, lang, ts):
 		'''
 		Callback that answer the user query
 		'''
@@ -66,9 +66,10 @@ class GNLP_Service:
 
 		self.KBC.addFact(self.ID, "NLP_ANSWER", 1, 50, {
 			"tag" : "NLP_ANSWER",
+			"language" : lang,
 			"text": answer,
 			"user_query": question,
-			"time_stamp": 1 # TODO
+			"time_stamp": ts # TODO
 			}
 		)
 

commit a6090265b5f0ed5c8d1401717f5cc158d6578f80

fixed a bug on timestamp field introduced in 08863d6d44f664c1dc47045393d9a505a2d47211

SmartApp.GNLP/gnlp_service.py
@@ -32,7 +32,7 @@ class GNLP_Service:
 		obj      = res[0]['details'][0]['object']['_data']
 		question = obj['text']
 		lang     = obj['language']
-		ts		 = obj['time_stamp']
+		ts		 = obj['timestamp']
 		print(question)
 		question = question['text']
 		luis_analysis = NLP_Understand(question, language = lang)
@@ -43,7 +43,7 @@ class GNLP_Service:
 			"entities": luis_analysis,
 			"dependencies": spacy_analysis,
 			"user_query": question,
-			"time_stamp": ts
+			"timestamp": ts
 			}
 		)
 		# Logging some infos
@@ -69,7 +69,7 @@ class GNLP_Service:
 			"language" : lang,
 			"text": answer,
 			"user_query": question,
-			"time_stamp": ts
+			"timestamp": ts
 			}
 		)
 
@@ -80,8 +80,8 @@ class GNLP_Service:
 		TAG_CRW_RAW_INFO = "CRAWLER_DATA_ENTRY"
 		TAG_REASONER_OUTPUT = "REASONING_FRAME"
 
-		self.KBC.subscribe(self.ID, {"_data": { "tag": TAG_USER_TRANSCRIPT ,"text": "$d", "language": "$lang"} }, self.analyse)
-		
+		self.KBC.subscribe(self.ID, {"_data": { "tag": TAG_USER_TRANSCRIPT ,"text": "$d", "language": "$lang", "timestamp": "$ts"} }, self.analyse)
+
 		print("Subscribed to the KB")
 
 